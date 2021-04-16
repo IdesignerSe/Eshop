485db4be39780676fb632d1e18fb8f2acd06b28e
idesignerse <heberandrade@gmail.com>
2021-04-16 15:29:11 +0200

    Update Startup.cs
    Ready for a new adventure

--- Eshop/EShop.Api/Startup.cs
+++ Eshop/EShop.Api/Startup.cs
@@ -28,10 +28,6 @@ namespace EShop.Api
         {
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "EShop.Api", Version = "v1" });
-            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,8 +36,6 @@ namespace EShop.Api
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "EShop.Api v1"));
             }
 
             app.UseHttpsRedirection();
