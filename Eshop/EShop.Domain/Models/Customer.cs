52438b6a94df1b1f6782daf1759f3fce8c091191
idesignerse <heberandrade@gmail.com>
2021-04-16 14:10:42 +0200

    Customer Class

---
+++ Eshop/EShop.Domain/Models/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EShop.Domain.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Age { get; set; }
+        public string PhotoUrl { get; set; }
+    }
+}
