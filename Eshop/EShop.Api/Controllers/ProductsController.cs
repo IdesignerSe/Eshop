9610b78fe94c731f6d1d14356bbaee1dbb182fd5
idesignerse <heberandrade@gmail.com>
2021-04-17 16:04:41 +0200

    Create 1st controller
    controller for Products &
    Remember import Using Microsoft .ASPNetCore.Mvc
    
    Adding controller to PoductsController

---
+++ Eshop/EShop.Api/Controllers/ProductsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EShop.Api.Controllers
+{
+    public class ProductsController : Controller
+    {
+
+    }
+}
