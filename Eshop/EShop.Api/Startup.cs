1dbc33b9c1912fe0250478b05ed1709ebed59db6
idesignerse <heberandrade@gmail.com>
2021-04-18 09:26:13 +0200

    Add Services in Startup.cs
    Add Services in Startup.cs
    -ISeeder in Interfaces and Seeder in Services
    -Never forget to add respective using statement

--- Eshop/EShop.Api/Startup.cs
+++ Eshop/EShop.Api/Startup.cs
@@ -1,3 +1,5 @@
+using EShop.Api.Interfaces;
+using EShop.Api.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -32,6 +34,7 @@ namespace EShop.Api
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "EShop.Api", Version = "v1" });
             });
+            services.AddScoped<ISeeder, Seeder>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
