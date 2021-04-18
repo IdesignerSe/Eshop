6f2bcf79a8ce2afe09762c4db7e5da34bf80c55d
idesignerse <heberandrade@gmail.com>
2021-04-18 09:10:07 +0200

    Product1 and 2 of List

--- Eshop/EShop.Api/Services/Seeder.cs
+++ Eshop/EShop.Api/Services/Seeder.cs
@@ -10,9 +10,27 @@ namespace EShop.Api.Services
 {
     public class Seeder : ISeeder
     {
+        private List<Product> _products = new List<Product>();
         public List<Product> GetAllProducts()
         {
-            throw new NotImplementedException();
+            var product1 = new Product
+            {
+                Id = 1,
+                Name = "Logitech IP Camera",
+                Description = "Does wonderful videos",
+                Price = 34.89f,
+            };
+            _products.Add(product1);
+
+            var product2 = new Product
+            {
+                Id = 2,
+                Name = "SuperDLux IP Camera",
+                Description = "Does Amazing Views",
+                Price = 56.10
+            };
+            _products.Add(product2);
+            return _products;
         }
     }
 }
